324f2322faec3a6b9c7ae7115c038538075f8550
ArthurNobrega13 <104452854+ArthurNobrega13@users.noreply.github.com>
2023-05-20 08:17:01 -0300

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -1,15 +1,15 @@
 package lista6;
 
-public class PessoaQ4 {
+public class Pessoa {
 	private String nome;
-	private DataQ4 dataNasc;
-	private EnderecoQ4 endereco;
-	private ContatoQ4 contato;
+	private Data dataNasc;
+	private Endereco endereco;
+	private Contato contato;
 	
-	public PessoaQ4() {
+	public Pessoa() {
 		
 	}
-	public PessoaQ4(String nome, DataQ4 dataNasc, EnderecoQ4 endereco, ContatoQ4 contato) {
+	public Pessoa(String nome, Data dataNasc, Endereco endereco, Contato contato) {
 		this();
 		this.nome = nome;
 		this.dataNasc = dataNasc;
@@ -23,19 +23,19 @@ public class PessoaQ4 {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public DataQ4 getDataNasc() {
+	public Data getDataNasc() {
 		return dataNasc;
 	}
 	public void setDataNasc(DataQ4 dataNasc) {
 		this.dataNasc = dataNasc;
 	}
-	public EnderecoQ4 getEndereco() {
+	public Endereco getEndereco() {
 		return endereco;
 	}
 	public void setEndereco(EnderecoQ4 endereco) {
 		this.endereco = endereco;
 	}
-	public ContatoQ4 getContato() {
+	public Contato getContato() {
 		return contato;
 	}
 	public void setContato(ContatoQ4 contato) {
